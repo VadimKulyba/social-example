bb53f489e6bd729ed156e6a993d36e241d5e7d70
Vadim <kulyba.vadim@gmail.com>
2018-01-08 03:43:26 +0300

    first work rspec

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.feature "static_pages_spec" do
   scenario "home" do
     visit '/static_pages/home'
-    expect(page).to have_title('Sample App')
+    expect(page).to have_content('Sample App')
   end
 end
