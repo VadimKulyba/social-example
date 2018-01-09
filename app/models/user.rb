bebcbcc1ada0e6bbf44acf16291bf23e25ea938e
Vadim <kulyba.vadim@gmail.com>
2018-01-10 02:54:58 +0300

    add user validation and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# Entity users
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  # validation exist
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates(:name, presence: true, length: { maximum: 50, minimum: 3 })
+  validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    length: { maximum: 25, minimum: 5 },
+                    uniqueness: { case_sensitive: false })
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: 'Example', email: 'example@gmail.com') }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  # show valid
+  it { should be_valid }
+
+  describe 'when name is not present' do
+    before { @user.name = ' ' }
+    it { should_not be_valid }
+  end
+  describe 'when name long' do
+    before { @user.name = 'a' * 51 }
+    it { should_not be_valid }
+  end
+  describe 'when email is not present' do
+    before { @user.email = ' ' }
+    it { should_not be_valid }
+  end
+  describe 'when email long' do
+    before { @user.email = 'a' * 26 }
+    it { should_not be_valid }
+  end
+  describe 'when email address is already taken' do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save # false if repeat
+    end
+
+    it { should_not be_valid }
+  end
 end
